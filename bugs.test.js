d9912757557e1b28390cba29dacb9a39dc549e83
Milena Daltiba <daltibamilena@gmail.com>
2022-03-06 13:23:05 -0400

    feat: add jest test and test post

---
+++ bugs.test.js
@@ -0,0 +1,72 @@
+const app = require("./app");
+const supertest = require("supertest");
+const request = supertest(app);
+
+test("Return empty array", async () => {
+  let response = await request.post("/bugs").send({
+    bugs: [
+      {
+        titulo: "Bug Test",
+        idade: 2,
+        estimativa: 4,
+        prioridade: "normal",
+      },
+    ],
+  });
+  expect(response.status).toBe(200);
+  expect(response.body).toStrictEqual([]);
+});
+
+test("Return bad request without send prioridade", async () => {
+  let response = await request.post("/bugs").send({
+    bugs: [
+      {
+        titulo: "Bug Test",
+        idade: 2,
+        estimativa: 4,
+      },
+    ],
+  });
+  expect(response.status).toBe(400);
+});
+
+test("Return bugs to resolve today", async () => {
+  let response = await request.post("/bugs").send({
+    bugs: [
+      {
+        titulo: "Bug Test",
+        idade: 2,
+        estimativa: 4,
+        prioridade: "critico",
+      },
+      {
+        titulo: "Bug Test 1",
+        idade: 2,
+        estimativa: 4,
+        prioridade: "normal",
+      },
+      {
+        titulo: "Bug Test 2",
+        idade: 3,
+        estimativa: 4,
+        prioridade: "normal",
+      },
+    ],
+  });
+    
+  expect(response.status).toBe(200);
+  expect(response.body).toEqual([
+    {
+      titulo: "Bug Test",
+      idade: 2,
+      estimativa: 4,
+      prioridade: "critico",
+    },
+    {
+      titulo: "Bug Test 2",
+      idade: 3,
+      estimativa: 4,
+      prioridade: "normal",
+    },
+  ]);
+});
--- controller/BugController.js
+++ controller/BugController.js
@@ -1,6 +1,7 @@
 const bugs_to_resolve = (req, res) => {
-  bugs = req.body.bugs;
-  res.send(bugs)
+  let bugs = req.body.bugs;
+
+  res.send(bugs.filter(({ prioridade, idade }) => prioridade === "critico" || idade === 3));
 };
 
 exports.bugs_to_resolve = bugs_to_resolve;
